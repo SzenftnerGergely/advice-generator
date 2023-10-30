b2de46d063ae34a1e3a8e50d60c7885003441bc7
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-30 14:24:38 +0100

    api call implemented, using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+type Advice = {
+  id: number,
+  advice: string
+}
 
 function App() {
+  const [advice, setAdvice] = useState<Advice | string>("");
+
+  const getAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        setAdvice(response.data.slip)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getAdvice()
+  },[])
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="max-w-xl flex flex-col items-center relative bg-[#313a49] p-8 rounded-lg">
-        <p className="text-[#3fdd8e] font-medium text-xl uppercase tracking-widest">Advice #117</p>
-        <p className="text-[#cee3e9] text-3xl text-center my-6">"It is easy to sit up and take notice, what's difficult is getting up and taking action."</p>
+      <div className="max-w-[540px] flex flex-col items-center relative bg-[#313a49] p-10 rounded-lg">
+        <p className="text-[#3fdd8e] font-semibold text-lg uppercase tracking-[0.3rem]">Advice #{advice.id}</p>
+        <p className="text-[#cee3e9] text-3xl text-center my-6 font-semibold leading-10">"{advice.advice}"</p>
         <img src="/pattern-divider-desktop.svg" alt="divider" className="mt-6 mb-8" />
-        <div 
+        <div
           className="bg-[#52ffa8] absolute -bottom-8 w-16 h-16 p-3.5 
           flex items-center justify-center rounded-full
           active:scale-95 cursor-pointer translate-all
           hover:shadow-[0px_0px_45px_6px_rgba(74,222,128,0.7)]
           "
+          onClick={getAdvice}
         >
           <img src="/icon-dice.svg" alt="dive icon" />
         </div>
