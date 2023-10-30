bb6c3d59ffac814ed863824aa1efbb9cc86a76dd
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-30 09:09:11 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col items-center  bg-[#313a49] p-8 rounded-lg">
+        <p>Advice #117</p>
+        <p>"It is easy to sit up and take notice, what's difficult is getting up adn taking action."</p>
+        <img src="/pattern-divider-desktop.svg" alt="divider" />
+        <div className="bg-[#52ffa8] w-12 h-12 p-3.5 flex items-center justify-center rounded-full">
+          <img src="/icon-dice.svg" alt="dive icon"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
