cf40f3cff6ff8e366c7f45e2a3acf6af1185bcef
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-11-01 11:00:05 +0100

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
-import Generator from "./component/Generator";
 
 type Advice = {
   id: number,
   advice: string,
-}
+} 
 
 function App() {
-  const [advice, setAdvice] = useState<Advice>("");
+  const [advice, setAdvice] = useState<Advice | null>(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false)
 
   const getAdvice = async () => {
     setLoading(true);
@@ -22,7 +20,6 @@ function App() {
       })
       .catch(error => {
         console.log(error);
-        setError(true)
       })
       .finally(() => {
         setLoading(false)
@@ -34,14 +31,38 @@ function App() {
   }, [])
 
   return (
-    <>
-      <Generator
-        advice={advice}
-        loading={loading}
-        error={error}
-        getAdvice={getAdvice}
-      />
-    </>
+    <div className="flex flex-col justify-center items-center h-screen">
+      {!advice ?
+      (<span className="loading loading-dots loading-lg"></span>) :
+      (<div
+        className="max-w-[540px] flex flex-col items-center 
+        relative bg-[#313a49] p-10 rounded-lg"
+      >
+        <p className="text-[#3fdd8e] font-semibold 
+         text-lg uppercase tracking-[0.3rem]"
+        >
+          Advice #{advice.id} 
+        </p>
+        {loading ?
+          <span className="loading loading-dots loading-lg"></span> :
+          <p className="text-[#cee3e9] text-3xl text-center 
+            my-6 font-semibold leading-10"
+          >
+            "{advice.advice}"
+          </p>}
+        <img src="/pattern-divider-desktop.svg" alt="divider" className="mt-6 mb-8" />
+        <div
+          className="bg-[#52ffa8] absolute -bottom-8 w-16 h-16 p-3.5 
+          flex items-center justify-center rounded-full
+          active:scale-95 cursor-pointer translate-all
+          hover:shadow-[0px_0px_45px_6px_rgba(74,222,128,0.7)]
+          "
+          onClick={getAdvice}
+        >
+          <img src="/icon-dice.svg" alt="dive icon" />
+        </div>
+      </div>)}
+    </div>
   )
 }
 
